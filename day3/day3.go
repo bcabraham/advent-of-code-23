fe5f9996e8fbdf3fe69fa69f0c875c4624f257ae
Bryan Abraham <babraham@gofigg.com>
2023-12-19 13:59:19 -0700

    day3 part2 solved

--- day3/day3.go
+++ day3/day3.go
@@ -22,21 +22,80 @@ func Run() {
 		log.Fatal(err)
 	}
 
-	tableWidth = len(lines[0])
+	var sum int
+	sum = sumAllPartNumbers(lines)
+	fmt.Println("Sum of all part numbers:", sum)
 
+	sum = calcGearRatios(lines)
+	fmt.Println("Sum of gear ratios:", sum)
+}
+
+func calcGearRatios(input []string) int {
+	fmt.Println("#### Calculating Gear Ratios ####")
+	tableWidth = len(input[0])
 	fmt.Println("tableWidth:", tableWidth)
 
-	data := strings.Join(lines, "")
+	data := strings.Join(input, "")
 	fmt.Println("input length:", len(data))
 
 	symbolRegex := regexp.MustCompile(`[^0-9\.\n]`)
 	symbolIndexes := symbolRegex.FindAllStringIndex(data, -1)
-
 	fmt.Printf("Symbol indexes: %d\n", len(symbolIndexes))
 
 	numericRegex := regexp.MustCompile(`(\d+)`)
 	numericIndexes := numericRegex.FindAllStringIndex(data, -1)
+	fmt.Printf("Numeric indexes: %d\n", len(numericIndexes))
 
+	sum := 0
+	for _, symbol := range symbolIndexes {
+		symbolIndex := symbol[0]
+		fmt.Printf("Searching adjacents to index: %d\n", symbolIndex)
+
+		searchIndexes := getAdjacentIndexes(symbolIndex, tableWidth)
+		fmt.Printf("Adjacent indexes: %+v\n", searchIndexes)
+
+		partNumbers := []int{}
+		for _, searchIndex := range searchIndexes {
+			// fmt.Println("Checking index:", searchIndex)
+			for i, numericIndex := range numericIndexes {
+				start, end := numericIndex[0], numericIndex[1]
+				if searchIndex >= start && searchIndex < end {
+					fmt.Printf("Match found:%d =< %d < %d ~ value: %s\n", start, searchIndex, end, data[start:end])
+					numericIndexes = removeAt(numericIndexes, i)
+
+					partNum, _ := strconv.Atoi(data[start:end])
+					partNumbers = append(partNumbers, partNum)
+				}
+			}
+		}
+
+		if len(partNumbers) < 2 {
+			fmt.Println("Not enough adjacent part numbers found")
+		} else if len(partNumbers) > 2 {
+			fmt.Println("Too many adjacent part numbers found")
+		} else {
+			product := partNumbers[0] * partNumbers[1]
+			sum += product
+			fmt.Printf("Calculating gear ratio: %d * %d = %d sum: %d\n", partNumbers[0], partNumbers[1], product, sum)
+		}
+	}
+
+	return sum
+}
+
+func sumAllPartNumbers(input []string) int {
+	tableWidth = len(input[0])
+	fmt.Println("tableWidth:", tableWidth)
+
+	data := strings.Join(input, "")
+	fmt.Println("input length:", len(data))
+
+	symbolRegex := regexp.MustCompile(`[*]`)
+	symbolIndexes := symbolRegex.FindAllStringIndex(data, -1)
+	fmt.Printf("Symbol indexes: %d\n", len(symbolIndexes))
+
+	numericRegex := regexp.MustCompile(`(\d+)`)
+	numericIndexes := numericRegex.FindAllStringIndex(data, -1)
 	fmt.Printf("Numeric indexes: %d\n", len(numericIndexes))
 
 	partNumbers := []int{}
@@ -47,7 +106,7 @@ func Run() {
 		searchIndexes := getAdjacentIndexes(symbolIndex, tableWidth)
 		fmt.Printf("Adjacent indexes: %+v\n", searchIndexes)
 		for _, searchIndex := range searchIndexes {
-			fmt.Println("Checking index:", searchIndex)
+			// fmt.Println("Checking index:", searchIndex)
 			for i, numericIndex := range numericIndexes {
 				start, end := numericIndex[0], numericIndex[1]
 				if searchIndex >= start && searchIndex < end {
@@ -66,7 +125,7 @@ func Run() {
 		sum += num
 	}
 
-	fmt.Println("Sum of all part numbers:", sum)
+	return sum
 }
 
 func getAdjacentIndexes(index, width int) []int {
